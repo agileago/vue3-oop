aea4170665c317a22a1a2f915ebfec4e9305a015
agileago <agileago@gmail.com>
2021-12-06 10:37:33 +0000

    Deploying to gh-pages from @ agileago/vue3-oop@50fb298046baeab7336343fcfee6f4e7816634fe 🚀

---
+++ assets/guide_component.md.bc2d8cdc.js
@@ -0,0 +1,10 @@
+import{_ as n,c as s,o as a,a as t}from"./app.4befc1fd.js";const m='{"title":"\u5B9A\u4E49","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5B9A\u4E49","slug":"\u5B9A\u4E49"}],"relativePath":"guide/component.md","lastUpdated":1638786997527}',e={},o=t(`<h2 id="\u5B9A\u4E49" tabindex="-1">\u5B9A\u4E49 <a class="header-anchor" href="#\u5B9A\u4E49" aria-hidden="true">#</a></h2><p>\u7EC4\u4EF6\u5FC5\u987B\u7EE7\u627F <strong><code>VueComponent</code></strong></p><div class="language-typescript"><pre><code><span class="token keyword">import</span> <span class="token punctuation">{</span> VueComponent <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39;vue3-oop&#39;</span>
+
+<span class="token keyword">class</span> <span class="token class-name">Foo</span> <span class="token keyword">extends</span> <span class="token class-name">VueComponent</span> <span class="token punctuation">{</span>
+  <span class="token function">render</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token punctuation">(</span>
+      <span class="token operator">&lt;</span>div<span class="token operator">&gt;</span>\u6211\u662F\u7EC4\u4EF6<span class="token constant">UI</span><span class="token operator">&lt;</span><span class="token operator">/</span>div<span class="token operator">&gt;</span>
+    <span class="token punctuation">)</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,3),p=[o];function c(r,l,i,d,u,k){return a(),s("div",null,p)}var f=n(e,[["render",c]]);export{m as __pageData,f as default};
