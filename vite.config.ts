6b46636446541200c5cdeee0f394aeb874f81835
agileago <agileago@gmail.com>
2021-12-17 17:01:34 +0800

    构建: 关闭esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vueJsx from '@vue3-oop/plugin-vue-jsx'
-import vue from '@vitejs/plugin-vue'
 import typescript from 'rollup-plugin-typescript2'
 // import typescript from '@rollup/plugin-typescript'
 
