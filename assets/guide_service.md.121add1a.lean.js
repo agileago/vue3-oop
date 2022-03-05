9a4d512a658d479872c44b56e165ece3b15f4346
agileago <agileago@gmail.com>
2022-03-05 07:54:29 +0000

    Deploying to gh-pages from @ agileago/vue3-oop@a6e206d71c79abb1793cefdc22300721873bdf46 🚀

---
+++ assets/guide_service.md.121add1a.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as s,o as a,a as t}from"./app.6e50d0d4.js";const v='{"title":"\u5B9A\u4E49","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5B9A\u4E49","slug":"\u5B9A\u4E49"}],"relativePath":"guide/service.md"}',p={},o=t("",8),e=[o];function c(u,l,k,i,r,d){return a(),s("div",null,e)}var f=n(p,[["render",c]]);export{v as __pageData,f as default};
